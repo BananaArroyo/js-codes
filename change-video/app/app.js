145767aa3bdf2e32e9768512fcb404fb5b4fdb08
Jesus Arroyo Olalde <puntoambar@Punto-Ambar-Yisus.local>
2021-06-07 15:51:34 -0500

    Cambiar video con JS

---
+++ change-video/app/app.js
@@ -0,0 +1,6 @@
+
+const video = document.querySelector("video")
+
+setTimeout( function(){
+    video.setAttribute('src', 'https://cdn.shopify.com/s/files/1/1744/3005/files/Carlo_Importaciones-Video_Institucional.mp4?v=1622747736')
+},3000)
--- test.js
+++ test.js
@@ -1,3 +1,85 @@
+const animation = document.querySelector('.custom-logo');
+        const page = document.querySelector('.page-id-6');
+        
+        function changeImg(animation, page){
+            if (page){
+                console.log("True");
+animation.src="https://puntoambar.com/PuntoAmbarwhite.png";
+            }
+        }
+
+changeImg(animation, page);
+
+
+//PROJECT EVENTS
+// Constante: galeria filtrable
+const galeryContainer = document.querySelector(".eael-filter-gallery-container")
+
+// Constante: botón retorno
+const toReturn = document.querySelector(".p-return")
+
+// ************ SOFIA RONQUILLO **********
+// Constantes: elemento para evento clic
+const pSofiaEvent = document.querySelector(".eael-cf-p-sofia")
+// Constantes: elemento para aplicar estilos (none/block)
+const pSofiaStyle = document.querySelector(".p-sofia")
+
+//Event: mostrar proyecto al hacer clic
+pSofiaEvent.onclick = showSofia // Ejecutar función
+
+// Función
+function showSofia ( evento ) {
+    pSofiaStyle.style.display = "block" //mostrar proyecto
+    galeryContainer.style.display = "none" //ocultar galeria filtrable
+    toReturn.style.display = "block"        //mostrar felcha retorno
+}
+
+// ************ FITBAY **********
+// Constantes: elemento para evento clic
+const pFitbayEvent = document.querySelector(".eael-cf-p-fitbay")
+// Constantes: elemento para aplicar estilos (none/block)
+const pFitbayStyle = document.querySelector(".p-fitbay")
+
+//Event: mostrar proyecto al hacer clic
+pFitbayEvent.onclick = showFitbay // Ejecutar función
+
+// Función
+function showFitbay ( evento ) {
+    pFitbayStyle.style.display = "block" //mostrar proyecto
+    galeryContainer.style.display = "none" //ocultar galeria filtrable
+    toReturn.style.display = "block"        //mostrar felcha retorno
+}
+
+// ************ TEMOC **********
+// Constantes: elemento para evento clic
+const pTemocEvent = document.querySelector(".eael-cf-p-temoc")
+// Constantes: elemento para aplicar estilos (none/block)
+const pTemocStyle = document.querySelector(".p-temoc")
+
+//Event: mostrar proyecto al hacer clic
+pTemocEvent.onclick = showTemoc // Ejecutar función
+
+// Función
+function showTemoc ( evento ) {
+    pTemocStyle.style.display = "block"     //mostrar proyecto
+    galeryContainer.style.display = "none"  //ocultar galeria filtrable
+    toReturn.style.display = "block"        //mostrar felcha retorno
+}
+
+//Colocarlo al FINAL
+toReturn.onclick = returnToGalery // Ejecutar función
+
+// Función
+function returnToGalery ( evento ) {
+    galeryContainer.style.display = "block" //mostrar galeria filtrable
+    //Ocultar proyectos
+    pSofiaStyle.style.display = "none"
+    pFitbayStyle.style.display = "none"
+    pTemocStyle.style.display = "none"
+    toReturn.style.display = "none"
+}
+
+// Add to card Workshops 
 const wPresencial = document.querySelector(".wPresencial")
 wPresencial.onclick = addToCartP
 
